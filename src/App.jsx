a328691c21657c83d79b915f050686e7beff04b7
gabrielberlucci <gabrielberluccidev@proton.me>
2026-01-16 02:21:55 -0300

    feat: add state lift up to pass props to new component resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,70 @@
+import { useState } from "react";
 import SideBar from "./components/SideBar";
+import Resume from "./components/Resume";
+import "./App.css";
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: "Your Name",
+    email: "youremail@email.com",
+    phone: "(11) 91234-5678",
+  });
+
+  const handlePersonalInfoChange = (e) => {
+    const { id, value } = e.target;
+    setPersonalInfo((prevInfo) => ({
+      ...prevInfo,
+      [id]: value,
+    }));
+  };
+
+  const [personalExperience, setPersonalExperience] = useState({
+    company: "Your Company",
+    position: "Your Position",
+    description: "Brief description",
+    startDate: "01-01-2001",
+    finalDate: "31-12-2022",
+  });
+
+  const handlePersonalExperienceChange = (e) => {
+    const { id, value } = e.target;
+    setPersonalExperience((prevInfo) => ({
+      ...prevInfo,
+      [id]: value,
+    }));
+  };
+
+  const [education, setEducation] = useState({
+    institution: "Your Institution",
+    course: "Your course",
+    startDate: "01-01-2001",
+    finalDate: "31-12-2022",
+  });
+
+  const handleEducationChange = (e) => {
+    const { id, value } = e.target;
+    setEducation((prevInfo) => ({
+      ...prevInfo,
+      [id]: value,
+    }));
+  };
+
   return (
-    <>
-      <SideBar />
-    </>
+    <div className="app-container">
+      <SideBar
+        onPersonalInfoChange={handlePersonalInfoChange}
+        personalInfo={personalInfo}
+        onPersonalExperienceChange={handlePersonalExperienceChange}
+        personalExperience={personalExperience}
+        onEducationChange={handleEducationChange}
+        education={education}
+      />
+      <Resume
+        personalInfo={personalInfo}
+        personalExperience={personalExperience}
+        education={education}
+      />
+    </div>
   );
 }
 
